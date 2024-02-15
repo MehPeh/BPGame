31cf733ec6258dd9bbe23312382d9ab849e05d9e
MehPeh <49045358+MehPeh@users.noreply.github.com>
2024-02-15 21:22:34 +0100

    First test run of node.js server

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 const WebSocket = require('ws')
 
 // Import Firebase Admin SDK
-const admin = require('firebase-admin'); 
-const serviceAccount = require('./bachelor-project-kdg-firebase-adminsdk-u4wkn-68efd3766b.json');
+// const admin = require('firebase-admin'); 
+// const serviceAccount = require('./bachelor-project-kdg-firebase-adminsdk-u4wkn-68efd3766b.json');
 
 // Initialize Firebase Admin SDK
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://bachelor-project-kdg-default-rtdb.europe-west1.firebasedatabase.app"
-});
+// admin.initializeApp({
+  // credential: admin.credential.cert(serviceAccount),
+  // databaseURL: "https://bachelor-project-kdg-default-rtdb.europe-west1.firebasedatabase.app"
+// });
 
 const wss = new WebSocket.Server({port:8080},()=>{
         console.log('Server started!')
@@ -20,8 +20,8 @@ wss.on('connection',(ws)=>{
                 console.log('data received %o', data.toString())
                 
                 // Use Firebase Admin SDK to interact with the Realtime Database
-                const db = admin.database();
-                const ref = db.ref('path/to/data'); // Replace with the actual path
+                // const db = admin.database();
+                // const ref = db.ref('path/to/data'); // Replace with the actual path
 
                 // Perform database interactions here
                 // For example, reading, writing, or listening for changes
